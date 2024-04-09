883aee685f9f88c8b1bc6267196020492b8b1bbb
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-04-09 11:51:02 -0500

    Se agrega el web socket

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import logger from "morgan";
 import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 dotenv.config();
 
@@ -8,6 +12,26 @@ const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+const db = createClient({
+  url: "libsql://cuddly-wasp-midudev.turso.io",
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`);
+
+io.on('connection', () => {
+  console.log("A user has connected!");
+})
 
 app.use(logger("dev"));
 
